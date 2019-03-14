3632290fa7583c24fbea69fcee27b907f86e2437
Felix Richter <judge@felixrichter.tech>
2019-03-15 00:03:58 +0100

    simple example to control 10 ws2812 leds

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
-
-#[link(name = "bcm2835", kind = "static")]
-extern "C" {
-	fn bcm2835_init() -> i32;
-	fn bcm2835_gpio_fsel(pin: u8, mode: u8);
-	fn bcm2835_gpio_write(pin: u8, state: u8);
-	fn bcm2835_delay(millis: u32);
-}
+use rs_ws281x::{ControllerBuilder, ChannelBuilder, StripType};
 
 fn main() {
+	let mut controller = ControllerBuilder::new()
+		.freq(800_000)
+		.dma(10)
+		.channel(
+		0,
+		ChannelBuilder::new()
+			.pin(18)
+			.count(10)
+			.strip_type(StripType::Ws2812)
+			.brightness(255)
+			.build()
+		)
+		.build().unwrap();
 
-    let pin = 17;
-    let mode = 1;
-    unsafe {
-		bcm2835_init();
-		bcm2835_gpio_fsel(pin, mode);
-    }
+	let leds = controller.leds_mut(0);
+	leds[0] = [255, 0, 0, 0];
+	leds[1] = [0, 255, 0, 0];
+	leds[2] = [0, 0, 255, 0];
+	leds[3] = [255, 255, 0, 0];
+	leds[4] = [255, 0, 255, 0];
+	leds[5] = [0, 255, 255, 0];
+	leds[6] = [255, 255, 255, 0];
+	leds[7] = [255, 0, 0, 0];
+	leds[8] = [0, 255, 0, 0];
+	leds[9] = [0, 0, 255, 0];
 
-    loop {
-	    unsafe {
-			bcm2835_gpio_write(pin, 1);
-			bcm2835_delay(500);
-			bcm2835_gpio_write(pin, 0);
-			bcm2835_delay(500);
-		}
-    }
+	controller.render();
 }
